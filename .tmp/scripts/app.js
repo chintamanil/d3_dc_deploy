46a27853c586d471054628a37b9ec6da9c68ce24
chintamanil <chintamani.lonkar@gmail.com>
2015-04-18 20:08:27 -0700

    updates

---
+++ .tmp/scripts/app.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+  var d3_data;
+
+  d3_data = angular.module("d3_data", ['angularDc', 'ngRoute', 'ui.bootstrap', 'firebase', 'angularChart', 'gridshore.c3js.chart']);
+
+}).call(this);
--- server.js
+++ server.js
@@ -1,48 +1,69 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var routes = require('./index');
+// var todos = require('./routes/todos');
+
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/todoApp', function(err) {
+//     if(err) {
+//         console.log('connection error', err);
+//     } else {
+//         console.log('connection successful');
+//     }
+// });
+
 var app = express();
 
-app.use(favicon());
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, '/')));
 
-/**
- * Development Settings
- */
-if (app.get('env') === 'development') {
+app.use('/', routes);
+//app.use('/todos', todos);
 
-	// This will change in production since we'll be using the dist folder
-	// This covers serving up the index page
-	app.use(express.static(path.join(__dirname, '../client/.tmp')));
-	app.use(express.static(path.join(__dirname, '../client/app')));
-
-}
-
-/**
- * Production Settings
- */
-if (app.get('env') === 'production') {
-
-	// changes it to use the optimized version for production
-	app.use(express.static(path.join(__dirname, '/')));
-
-}
-
-/**
- * Routes
- */
-// var router = require('./router')(app);
-
-// Error Handling
-app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
 
 module.exports = app;
